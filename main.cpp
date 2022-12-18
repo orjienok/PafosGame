842f6c99985cd6eb82636a747376e71dd092de82
klodzonov <klodzonov@gmail.com>
2022-12-18 13:01:25 +0500

    Updates

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "Window.h";
+#include "Core/Window.h";
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
 int WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
